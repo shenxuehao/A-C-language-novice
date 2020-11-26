b4b4813e15d1d574d23a23cb598872d84d258cb4
shenxuehao <2398444863@qq.com>
2020-11-26 09:00:14 +0800

    2020-11-26

---
+++ 2020-11-9/2020-11-9/test.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int main()
+{
+	int a[5] = { 1, 2, 3, 4, 5 };
+	int* ptr = (int*)(&a + 1);
+	printf("%d,%d", *(a + 1), *(ptr - 1));
+	return 0;
+}//2 5	
