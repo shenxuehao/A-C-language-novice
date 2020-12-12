d53728dd02d0c613eaf299d72785a8bef97fcf3e
shenxuehao <2398444863@qq.com>
2020-12-12 15:01:17 +0800

    2020-12-12

---
+++ 2020-11-23（2）/2020-11-23（2）/test.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int main()
+{
+	int A=0, B, C, D, E;
+	while (((C != 1) && (E - D == 2) && (E != 2) && (A != 1) && (A != 5) && (B - C == 1) && (A >= 1 && A <= 5) && (B >= 1 && B <= 5) && (C >= 1 && C <= 5) && (D >= 1 && D <= 5) && (E >= 1 && E <= 5) && (A != B) && (A != C) && (A != D) && (A != E) && (B != C) && (B != D) && (B != E) && (C != D) && (C != E) && (D != E))==1);
+	printf("A=%d,B=%d,C=%d,D=%d,E=%d", A, B, C, D, E);
+	return 0;
+}
