b811e7abaa0d2fac6f3bb4fc38a5bda4666d1a0d
shenxuehao <2398444863@qq.com>
2020-12-18 16:20:54 +0800

    2020-12-18

---
+++ 2020-12-1/2020-12-1/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+//struct S
+//{
+//	int n;
+//	int arr[0];
+//};
+//int main()
+//{
+//	//struct S s;
+//	//printf("%d\n", sizeof(s));
+//	struct S* ps = (struct S*)malloc(sizeof(struct S) + 5 * sizeof(int));//开辟好了24个字节
+//	ps->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;//0 1 2 3 4
+//	}
+//	struct S *ptr = realloc(ps, 44);
+//	if (ptr != NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ",ps->arr[i]);
+//	}
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
+struct S
+{
+	int n;
+	int* arr;;
+};
+int main()
+{
+	struct S*ps = (struct S*)(malloc(sizeof(struct S)));
+	return 0;
+}
