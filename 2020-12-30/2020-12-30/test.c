d7a5edf311af406d0fe6ef72ab5a5296e0608fa5
shenxuehao <2398444863@qq.com>
2021-01-01 18:42:14 +0800

    2021-1-1

---
+++ 2020-12-30/2020-12-30/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int main()
+{
+	int a = 1234;
+	printf("%o\n", a);
+	printf("%x\n", a);
+	return 0;
+}
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n",sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("     **     \n     **     \n************\n************\n    *  *    \n    *  *    \n");
+//	return 0;
+//}
