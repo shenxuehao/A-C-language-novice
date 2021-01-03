9ef19de7832787dce25d49b8e41793128d6d43e8
shenxuehao <2398444863@qq.com>
2021-01-03 20:16:40 +0800

    2021-1-3

---
+++ code_python/2020-12-30bianliang.py
@@ -0,0 +1,4 @@
+my_name = 'tom'
+print(my_name)
+schoolName = '南京农业大学'
+print(schoolName)
---
+++ code_python/2020-12-30zhushi.py
@@ -0,0 +1,15 @@
+# 单行，输出hello world
+print("hello world")
+
+print("hello python")
+
+"""
+第一行注释
+第二行注释
+第三行注释
+"""
+'''
+注释
+注释
+注释
+'''
