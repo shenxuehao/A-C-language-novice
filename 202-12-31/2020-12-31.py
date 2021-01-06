fbb5f7991535ed39b2b517ef4c264dffeee19a13
shenxuehao <2398444863@qq.com>
2021-01-06 09:40:45 +0800

    2021-1-6

---
+++ 202-12-31/2020-12-31.py
@@ -0,0 +1,12 @@
+print("hello python")
+age=18
+print(age)
+age = 18
+name = 'tom'
+weight = 75.5
+student_id = 1
+print("我的名字是%s" %name)
+print("我的学号是%4d"%student_id)
+print("我的体重是%.2f公斤"%weight)
+print("我的名字是%s,今年%d岁了"%(name,age))
+
