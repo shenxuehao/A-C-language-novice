990db68a4ca3ec60c159ce17686061f1cbe8f2ae
shenxuehao <2398444863@qq.com>
2021-01-08 09:21:44 +0800

    2021-1-8

---
+++ 2021-1-3/2021-1-3/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_DEPRECATE
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		printf("%d",1 << n);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int date = 0;
+//	scanf("%4d%2d%2d", &year, &month, &date);
+//	printf("year=%4d\n", year);
+//	printf("month=%02d\n", month);
+//	printf("date=%02d\n", date);
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[12] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%c", arr[i]);
+//	}
+//	return 0;
+//}
