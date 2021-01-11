716bf0740031d5b7728e1bdc57940562ab05fbd5
shenxuehao <2398444863@qq.com>
2021-01-11 09:03:31 +0800

    2021-1-11

---
+++ 2021-1-1/2021-1-1/test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int main()
+{
+	int ret = printf("Hello world!");
+	printf("%d\n", ret);
+	return 0;
+}
+
+//int main()
+//{
+//	int a = 0XABCDEF;
+//	printf("%15d", a);
+//	return 0;
+//}
