fa4e4f1f144f6573c515d8a8242a2e514b3fd2df
shenxuehao <2398444863@qq.com>
2021-01-24 09:55:28 +0800

    2021-1-24

---
+++ 2021-1-3(1)/test.py
@@ -0,0 +1,15 @@
+a=10
+a+=1
+print(a) #11
+
+b=10
+b-=1
+print(b)
+
+c=10
+c+=c+1
+print(c)
+
+d=10
+d*=1+2
+print(d)
---
+++ 2021-1-3（2）/test1.py
@@ -0,0 +1,6 @@
+age=int(input("请输入您的年龄"))
+if age >= 18:
+    print(f"您的年龄是{age}，已经成年，可以上网")
+else:
+    print("未成年不许上网")
+print("系统关闭")
