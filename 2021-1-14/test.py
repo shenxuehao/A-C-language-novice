38a55d4fb5f018c23cb7b9777de56dca476bebfd
shenxuehao <2398444863@qq.com>
2021-02-01 18:32:07 +0800

    2021-2-1

---
+++ 2021-1-14/test.py
@@ -0,0 +1,9 @@
+"""
+def user_info(name,age,gender):
+    print(f'{name},{age},{gender}')
+
+user_info('Rose',age=20,gender='female')
+user_info('小明',gender='male',age=16)
+"""
+print(type('TOM'))
+print(type(('TOM','ton')))
