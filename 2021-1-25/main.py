7ce1b6e786d4b1162b2f31ac0ddab0495552342d
shenxuehao <2398444863@qq.com>
2021-02-13 18:07:18 +0800

    2021-2-13

---
+++ 2021-1-25/main.py
@@ -0,0 +1,39 @@
+class Master(object):
+    def __init__(self):
+        self.kongfu = '[古法煎饼果子配方]'
+
+    def make_cake(self):
+        print(f'运用{self.kongfu}制作煎饼果子')
+
+class School(object):
+    def __init__(self):
+        self.kongfu = '[黑马煎饼果子配方]'
+
+    def make_cake(self):
+        print(f'运用{self.kongfu}制作煎饼果子')
+
+class Prentice(School, Master):
+    def __init__(self):
+        self.kongfu = '[独创煎饼果子配方]'
+
+    def make_cake(self):
+        self.__init__()
+        print(f'运用{self.kongfu}制作煎饼果子')
+
+    def make_master_cake(self):
+        Master.__init__(self)
+        Master.make_cake(self)
+
+    def make_school_cake(self):
+        School.__init__(self)
+        School.make_cake(self)
+
+daqiu = Prentice()
+
+daqiu.make_cake()
+
+daqiu.make_master_cake()
+
+daqiu.make_school_cake()
+
+daqiu.make_cake()
