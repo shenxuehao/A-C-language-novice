5d4c783d31f33e53288cddb2c04586b22cb96006
shenxuehao <2398444863@qq.com>
2021-04-09 19:56:19 +0800

    2021-4-9

---
+++ 2021-3-7(0)/2021-3-7(0)/源.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MaxSize 100
+typedef struct
+{
+	int data;
+	int parent;
+}PTNode;
+
+typedef struct
+{
+	PTNode nodes[MaxSize];
+	int n;
+}PTree;
+
+void test()
+{
+
+}
+
+int main()
+{
+	test();
+	return 0;
+}
